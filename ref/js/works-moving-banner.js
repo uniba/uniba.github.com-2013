023ce3304de5f8cd22955aed282b3006eb186cfa
Keiichi Tanifuji <keiichi@uniba.jp>
2012-04-04 14:13:00 +0900

    CSS,js修正

--- ref/js/works-moving-banner.js
+++ ref/js/works-moving-banner.js
@@ -15,13 +15,15 @@ $(function(){
 	$('.works-block.move-bottom').hover(function(){
 		var $this = $(this);
 		$this.css("z-index","10").addClass("active");
-		$this.find(".works-detail").stop().animate({height:"160px"}, 200, function(){$this.find(".works-photo").addClass("active");}).animate({top:"-160px"}, 200);
+		
+		$this.find(".works-detail").stop(true,false).animate({height:"160px"}, 200, function(){$this.find(".works-photo").addClass("active");}).animate({top:"-160px"}, 200);
+		
 		$this.find(".works-detail table tr td").css("display","block");
 		$this.find(".works-detail table tr td p").css("display","block");
 	}, function(){
 		var $this = $(this);
 		$this.css("z-index","1").removeClass("active");
-		$this.find(".works-detail").stop().css("z-index","1").animate({height:"70px", top:"0px"}, 200);
+		$this.find(".works-detail").stop(true,false).css("z-index","1").animate({height:"70px", top:"0px"}, 200);
 		$this.find(".works-detail table tr td").css("display","none");
 		$this.find(".works-detail table tr td p").css("display","none");
 		$this.find(".works-photo").removeClass("active");
@@ -30,13 +32,13 @@ $(function(){
 	$('.works-block.move-right').hover(function(){
 		var $this = $(this);
 		$this.css("z-index","10").addClass("active");
-		$this.find(".works-detail").stop().animate({height:"160px"}, 200, function(){$this.find(".works-photo").addClass("active");}).animate({right:"180px"}, 200);	
+		$this.find(".works-detail").stop(true,false).animate({height:"160px"}, 200, function(){$this.find(".works-photo").addClass("active");}).animate({right:"180px"}, 200);	
 		$this.find(".works-detail table tr td").css("display","block");
 		$this.find(".works-detail table tr td p").css("display","block");
 	}, function(){
 		var $this = $(this);
 		$this.css("z-index","1").removeClass("active");
-		$this.find(".works-detail").stop().animate({height:"70px", right:"0px"}, 200);
+		$this.find(".works-detail").stop(true,false).animate({height:"70px", right:"0px"}, 200);
 		$this.find(".works-detail table tr td").css("display","none");
 		$this.find(".works-detail table tr td p").css("display","none");
 		$this.find(".works-photo").removeClass("active");
