26036a02b6be41791205fe12a7288b5fdb3af530
soundofdelight <soundofdelight@gmail.com>
2012-07-28 19:13:10 +0900

    色々調整

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,5 @@
 	$(function(){
 	
-   var rotateCont = $('#aRotation'),
-       Interval = 100,
-       animPosition = 10,
-       animWidth = 200
- 
-   setInterval(rotateimg, Interval);
-   
-   function rotateimg(){
-     i = 1;
-     var rotateEl = rotateCont.find('li');
-     var el = rotateEl.eq(0);
-      rotateCont.stop().animate({right: '+=' + animPosition + 'px' },animWidth); 
-   }
-	
  	 $('#nav').parent().waypoint(function(event, direction) {
 	 	  $(this).toggleClass('fixed', direction === "down");
 	 	  event.stopPropagation();
@@ -21,6 +7,7 @@
 	  
 	  $('section').waypoint(function(event, direction) {
 	    var $active = $(this);
+	    var atr = $('a[href=#'+$active.attr('id')+']');
 	    if (direction === "up") {
  	     $active = $active.prev();
  	   }
@@ -28,13 +15,36 @@
 	    $('a[href=#'+$active.attr('id')+']').addClass('active');
 	  });
 	  
+/*
+	  $('#about').waypoint(function(event, direction) {
+	    $('.rocket').animate({right:0},2000,'easeOutQuad');
+	    setTimeout(function(){
+ 	     setInterval(rotateimg, Interval);
+	    }, 1200);
+	  });
+*/
+	  
+	  setInterval(rotateimg, Interval);
+	  
+   var rotateCont = $('#aRotation'),
+       Interval = 100,
+       animPosition = 10,
+       animWidth = 200
+   
+   function rotateimg(){
+     i = 1;
+     var rotateEl = rotateCont.find('li');
+     var el = rotateEl.eq(0);
+      rotateCont.stop().animate({right: '+=' + animPosition + 'px' },animWidth); 
+   }
+	  
   	$('a[href*=#]').click(function() {
   		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
   			var $target = $(this.hash);
   				$target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
   			if ($target.length) {
   				var targetOffset = $target.offset().top;
-  				$('html,body').animate({scrollTop: targetOffset}, {duration:200},'easeInExpo');
+  				$('html,body').animate({scrollTop: targetOffset}, {duration:500},'easeInOutExpo');
   				return false;
   			}
   		}
