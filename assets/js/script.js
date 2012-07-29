ae56e13d3e6151fa456a8898ff8314c6f914e25c
nulltask <nulltask@gmail.com>
2012-07-29 19:47:25 +0900

    add more comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,34 @@
 $(function() {
-  
+
+  /**
+   * DOM dependencies.
+   */
+
   var $window = $(window)
     , $nav = $('#nav')
     , $footer = $('#footer');
-  
+
   /**
    * Fixed navigation.
    */
-  
+
   $nav.parent().waypoint(function(event, direction) {
     $(this).toggleClass('fixed', direction === "down");
     event.stopPropagation();
   });
-  
+
   /**
    * Initialize footer size.
    */
-  
+
   $footer.css({
       height: $window.height() - $nav.height()
   });
-  
+
   /**
    * Scroll spy.
    */
-  
+
   $('section').waypoint(function(event, direction) {
     var $active = $(this);
     var atr = $('a[href=#'+$active.attr('id')+']');
@@ -34,10 +38,16 @@ $(function() {
     $('.active').removeClass('active');
     $('a[href=#'+$active.attr('id')+']').addClass('active');
   });
-  
-  
+
+
+  /**
+   * Scaling transition on hover
+   *
+   * TODO: may be it is obtrusive script. should move to stylesheets.
+   */
+
   $('.scale img,#cliantGrid img').scale(0.9);
-  
+
   $('.scale img,#cliantGrid img').hover(function() {
     $(this).scale(1);
     },function(){
@@ -48,30 +58,36 @@ $(function() {
     },function(){
       $(this).prev().css('color','black');
     });
-    
+
   $('.close').click(function(){
     $(this).parent().hide();
   });
-    
+
+  /**
+   * Rotation images.
+   *
+   * TODO: migrate to jquery plugin pattern. it may be better.
+   */
+
   var $rotateCont = $('#aRotation'),
       $rotateChild = $rotateCont.children();
       $rotateWidth = $('#aRotation').children().width();
-  
+
   console.log($rotateWidth);
   $rotateChild.clone().appendTo($rotateCont);
-  
+
   function rotateimg () {
-    $rotateCont.stop().animate({right: '+=' + 1 + 'px' },0); 
+    $rotateCont.stop().animate({right: '+=' + 1 + 'px' },0);
   }
-   
+
   function startTimer () {
     timer = setInterval(rotateimg, 20);
   }
-   
+
   function stopTimer () {
     clearInterval(timer);
   }
-   
+
   $rotateCont.on({
     mouseenter: function () {
       stopTimer();
@@ -80,13 +96,19 @@ $(function() {
       startTimer();
     }
   });
-   
+
   timer = setInterval(rotateimg, 20);
-   
+
+  /**
+   * Works category filtring.
+   *
+   * TODO: masonry plugin + css3 transition is much better?
+   */
+
   var $controls = $('#filter li'),
       $list = $('.worksGrid'),
       $data = $list.clone();
-    
+
   var $preferences = {
     duration: 600,
     easing: 'easeInOutQuad',
@@ -96,28 +118,32 @@ $(function() {
       return $(v).find('img').attr('src');
     }
   };
-  
+
   $controls.each(function(i) {
     $(this).find('a').bind('click', function(e) {
       var $button = $(this);
-      var $button_container = $button.parent();     
+      var $button_container = $button.parent();
 
       $('#filter li').removeClass('selected');
       $button_container.addClass('selected');
       var sorting_kind = $button.data('value');
-      
+
       if(!$button.hasClass('selected')) {
         if (sorting_kind == 'all') {
           var $filtered_data = $data.find('li');
         } else {
           var $filtered_data = $data.find('li.' + sorting_kind);
         }
-        
+
         $list.quicksand($filtered_data, $preferences);
       }
       e.preventDefault();
     });
-  }); 
+  });
+
+  /**
+   * In-page anchor.
+   */
 
   $('a[href*=#]').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -135,15 +161,15 @@ $(function() {
 $(function() {
   var $nav = $('nav#nav')
     , $window = $(window);
-  
+
   /**
    * Resize logo and background.
    */
-  
+
   $window.on('resize', function(e) {
     var width = $window.width()
       , height = $window.height();
-    
+
     if (renderer) {
       renderer.setSize(width, height);
     }
@@ -151,7 +177,7 @@ $(function() {
       // TODO: avoid flicking.
       p5.setSize(width, height);
     }
-  }).trigger('resize');  
+  }).trigger('resize');
 });
 
 /**
@@ -165,10 +191,10 @@ $(window).on('keypress', function(e) {
       , ctx = offscreenCanvas.getContext('2d')
       , logo = $('#webglcontainer').find('canvas').get(0)
       , background = $('#p5container').get(0);
-    
+
     offscreenCanvas.width = background.width;
     offscreenCanvas.height = background.height;
-    
+
     ctx.drawImage(background, 0, 0, background.width, background.height);
     ctx.drawImage(logo, 0, 0, logo.width, logo.height);
     window.open(offscreenCanvas.toDataURL());
