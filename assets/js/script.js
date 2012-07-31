04cbda6f989f15b16586b5fdd6d37e71b4a7436a
soundofdelight <soundofdelight@gmail.com>
2012-07-31 19:24:53 +0900

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -164,32 +164,17 @@ $(function() {
     }
   });
 
-	$('.max #visual #pNav li').live(
-		{
-			'mouseenter':function(){
-				$('#pNavWall').show();
-				$(this).find('.under').show();
-				$(this).addClass('hover');
-			},
-			'mouseleave':function(){
-				$(this).find('.under').hide();
-				$(this).removeClass('hover');
-			}
-	});
-
 	$('.worksGrid li').live(
 			'mouseleave',function(){
-					$('.worksGrid li').delay(100).animate({opacity:'1'},200);
-					$('.worksGrid li').find('.description').animate({bottom:'-100px'},100,'easeOutExpo');
+					 $('.worksGrid li').delay(100).removeClass('workActive');
+					 $('.worksGrid li').delay(100).removeClass('workOther');
 	});
 
 	$('.worksGrid li').live(
 			'mouseenter',function(){
-				$('.worksGrid li').stop(true,true).not(this).animate({opacity:'0.7'},200);
-				$(this).animate({opacity:'1'},200);
-					$(this).find('.description').animate({bottom:'0px'},100,'easeOutExpo');
+				  $('.worksGrid li').not(this).addClass('workOther');
+				  $(this).addClass('workActive');
 	});
-  
   
 });
 
