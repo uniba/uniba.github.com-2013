c04b63878366b2faba9b4fed110f73543d89c93d
takumiabe <takumiabe2@gmail.com>
2013-04-05 21:36:53 +0900

    exhibitionサイト修正

--- futuresemaphore/js/script.js
+++ futuresemaphore/js/script.js
@@ -8,27 +8,57 @@ $(function() {
     return ((tileheight) - (Math.floor(scrollposition / speedratio) % (tileheight+1)));
   }
 
+  var ground = $("#ground");
+
+  $(function(){
+    var bgWidth = $(window).width();
+
+    if(bgWidth < 1280){
+      $(ground).css({backgroundSize:"1280px"});
+    }else{
+      $(ground).css({backgroundSize:"100%"});
+    }
+  });
+
   window.onload = (function() {
     window.onscroll = (function() {
+
+      var bgWidth = $(window).width();
       var posY = (document.documentElement.scrollTop) ? document.documentElement.scrollTop : window.pageYOffset;
 
       var ground = document.getElementById('ground');
-      var groundparallax = calcParallax(3020, 1.5, posY);
+      var groundparallax = calcParallax(1933, 10, posY);
       ground.style.backgroundPosition = "0 " + groundparallax + "px";
+
+      if(bgWidth < 1280){
+      $(ground).css({backgroundSize:"1280px"});
+    }else{
+      $(ground).css({backgroundSize:"100%"});
+    }
+
     });
   });
+
+  // $(function(){
+  //   var bg01 = $("#ground").css("height").replace("px","");
+  //   $(window).scroll(function(){
+  //     var y = $(this).scrollTop();
+  //     $("#ground").css('background-position','0px ' + parseInt( bg01 - y * 0.1) + 'px');
+  //   });
+  // });
 
   $(window).resize(function(){
     var bgWidth = $(window).width();
     var bgHeight = $(window).height();
 
+    $("#ground").css({"top":"0"});
     $("p5_canvas").css({width:bgWidth,height:bgHeight});
 
-    var parallaxBg = $(".parallax-bg");
+    var ground = $("#ground");
     if(bgWidth < 1280){
-      $(parallaxBg).css({backgroundSize:"1280px"});
+      $(ground).css({backgroundSize:"1280px"});
     }else{
-      $(parallaxBg).css({backgroundSize:"100%"});
+      $(ground).css({backgroundSize:"100%"});
     }
   });
 
