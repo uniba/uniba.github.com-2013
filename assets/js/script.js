54875e41138c1c0f35119f72df33703c5b1579d9
tetsuro <tetsuro@uniba.jp>
2013-04-08 20:11:33 +0900

    トップ画像が切れる問題を修正

--- assets/js/script.js
+++ assets/js/script.js
@@ -197,6 +197,27 @@ $(function() {
 	
 });
 
+$(function() {  
+    var theWindow = $(window),
+    $bg = $("#exhibision2013"),
+    aspectRatio = $bg.width() / $bg.height();
+ 
+    function resizeBg() {
+        if ( (theWindow.width() / theWindow.height()) < aspectRatio ) {
+            $bg
+                .removeClass()
+                .addClass('bgheight');
+        } else {
+            $bg
+                .removeClass()
+                .addClass('bgwidth');
+        }
+    }
+    theWindow.resize(function() {
+        resizeBg();
+    }).trigger("resize");
+});
+
 $(function() {
   var $nav = $('nav#nav')
     , $window = $(window);
